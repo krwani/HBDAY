6cbc3829bc5499ea148f3a79cbbf15c3eb13d936
krwani <kalpeshwani1@gmail.com>
2016-05-22 16:53:09 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,15 +1,16 @@
 function myFunction() {
-    var x;
+    var x, output;
     x = document.getElementById("input").value;
-    if (x==="events"||(x==="Events"||(x==="EVENTS") {
+    if (x==="events") {
         window.open("http://technovanza.org/autobots.php");
     } 
-    else if(x==="sponsors"||(x==="Sponsors"||(x==="SPONSORS"){
+    else if(x==="sponsors"){
         window.open("http://technovanza.org/sponsors.php");
     }
-    else if(x==="map"||(x==="Map"||(x==="MAP"){
+    else if(x==="map"){
         window.open("https://www.google.co.in/maps/place/Veermata+Jijabai+Technological+Institute/@19.0222386,72.8539803,17z/data=!3m1!4b1!4m5!3m4!1s0x3be7cf268c5e833d:0xd2be7bbd1ee0e0a1!8m2!3d19.0222335!4d72.856169?hl=en");
     }
     else{
     window.alert("Invalid input");}
+    document.getElementById("demo").innerHTML = output;
 }
